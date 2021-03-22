4effa81ef551c6c6ae2c39c19f06370bdebb4dd1
porcello <peng0406@yahoo.ne.jp>
2021-03-22 14:59:49 +0800

    mission22_3

--- mission22/script.js
+++ mission22/script.js
@@ -1,7 +1,8 @@
 function caclulate() {
     fetch('items.json')
         .then(res => res.json())
-        .then(data =>console.log(data));
+        .then(data => (document.body.innerHTML = data[0].text))
+        ;
 }
 
 caclulate();
