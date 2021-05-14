ed209b9809863566b8e2f9d9c447d813567b5dc5
porcello <peng0406@yahoo.ne.jp>
2021-05-14 16:54:53 +0800

    mission29

--- mission29/script.js
+++ mission29/script.js
@@ -4,9 +4,19 @@ const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
+
+// Upadte countdown time
+
 
 function updateCountdown() {
     const currentTime = new Date();
@@ -23,5 +33,11 @@ function updateCountdown() {
     seconds.innerHTML = s < 10 ? '0' + s : s;
 
 }
+// Show spinner before countdown
+setTimeout(() =>{
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
 
+// Run every second
 setInterval(updateCountdown, 1000);
